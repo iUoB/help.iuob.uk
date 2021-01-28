1512353d3c8903fbb7e4a8aae30ef9932b849f11
Harry-Yep <hi@hiio.me>
2021-01-28 18:11:13 +0000

    edit config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    permalink: "/post/:slug",
+    dest: 'public',
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }],
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
