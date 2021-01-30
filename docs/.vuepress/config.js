8540313f23fa1c8394d5f17290a3ad9e18cf3497
Harry-Yep <hi@hiio.me>
2021-01-30 04:30:48 +0000

    change Pics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
         ['meta', { name: 'keywords', content: 'iUoB, University of Birmingham, UoB, 伯明翰大学' }],
         ['link', { rel: 'icon', href: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' }],
         ['link', { rel: 'icon', href: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' }],
-        ['link', { rel: 'apple-touch-icon', href: '/favicon-32x32.png', sizes: '180x180' }],
+        ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
         ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#42b983' }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
