caf4a2336b7b4b1747c7f34b9af07258a5d50025
Harry Yep <github@hode.uk>
2021-04-02 15:01:34 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,28 +2,62 @@ module.exports = {
     theme: 'default-prefers-color-scheme',
     dest: 'public',
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }],
-        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ['meta', { name: 'author', content: 'Harry Yep' }],
-        ['meta', { name: 'keywords', content: 'iUoB, University of Birmingham, UoB, 伯明翰大学' }],
-        ['link', { rel: 'icon', href: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' }],
-        ['link', { rel: 'icon', href: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' }],
-        ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
-        ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#42b983' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+        ['link', {
+            rel: 'icon',
+            href: '/favicon.ico'
+        }],
+        ['meta', {
+            name: 'viewport',
+            content: 'width=device-width,initial-scale=1,user-scalable=no'
+        }],
+        ['meta', {
+            name: 'author',
+            content: 'Harry Yep'
+        }],
+        ['meta', {
+            name: 'keywords',
+            content: 'iUoB, University of Birmingham, UoB, 伯明翰大学'
+        }],
+        ['link', {
+            rel: 'icon',
+            href: '/favicon-16x16.png',
+            type: 'image/png',
+            sizes: '16x16'
+        }],
+        ['link', {
+            rel: 'icon',
+            href: '/favicon-32x32.png',
+            type: 'image/png',
+            sizes: '32x32'
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            href: '/apple-touch-icon.png',
+            sizes: '180x180'
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: '/safari-pinned-tab.svg',
+            color: '#42b983'
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/manifest.json'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }]
     ],
     locales: {
         '/': {
             lang: 'zh-CN',
             title: '帮助中心 - iUoB',
             description: '为你通往伯明翰大学的路上亮起明灯',
-        },
-        '/en-GB/': {
-            lang: 'en-GB',
-            title: 'Help Center - iUoB',
-            description: 'Light up your way to the University of Birmingham',
         }
     },
     plugins: [
@@ -71,125 +105,116 @@ module.exports = {
                 repoLabel: '查看源码',
                 editLinks: true,
                 editLinkText: '在GitHub编辑此页面',
-                nav: [
-                    { text: '伯明翰大学官网', link: 'https://www.birmingham.ac.uk/' },
-                    { text: '主页', link: '/' },
-                    { text: '详情', link: '/details/' },
+                nav: [{
+                        text: '伯明翰大学官网',
+                        link: 'https://www.birmingham.ac.uk/'
+                    },
+                    {
+                        text: '主页',
+                        link: '/'
+                    },
+                    {
+                        text: '详情',
+                        link: '/details/'
+                    },
                     {
                         text: '问题选择',
                         ariaLabel: '问题选择',
                         items: [{
                                 text: '入学前',
-                                items: [
-                                    { text: '详情', link: '/details/pre-admissions/' },
-                                    { text: '录取相关', link: '/details/pre-admissions/admissions/' },
-                                    { text: '联系方式', link: '/details/pre-admissions/contacts/' },
-                                    { text: '其他', link: '/details/pre-admissions/others/' }
+                                items: [{
+                                        text: '详情',
+                                        link: '/details/pre-admissions/'
+                                    },
+                                    {
+                                        text: '录取相关',
+                                        link: '/details/pre-admissions/admissions/'
+                                    },
+                                    {
+                                        text: '联系方式',
+                                        link: '/details/pre-admissions/contacts/'
+                                    },
+                                    {
+                                        text: '其他',
+                                        link: '/details/pre-admissions/others/'
+                                    }
                                 ]
                             },
                             {
                                 text: '入学后',
-                                items: [
-                                    { text: '详情', link: '/details/enrolled/' },
-                                    { text: 'Solutions', link: '/details/enrolled/solutions/' },
-                                    { text: '其他', link: '/details/enrolled/others/' }
+                                items: [{
+                                        text: '详情',
+                                        link: '/details/enrolled/'
+                                    },
+                                    {
+                                        text: 'Solutions',
+                                        link: '/details/enrolled/solutions/'
+                                    },
+                                    {
+                                        text: '其他',
+                                        link: '/details/enrolled/others/'
+                                    }
                                 ]
                             },
                             {
                                 text: '其他问题',
-                                items: [
-                                    { text: '详情', link: '/details/others/' },
-                                    { text: '关键词查询', link: '/details/others/keywords/' },
-                                    { text: '未整理问题', link: '/details/others/unorganized/' },
+                                items: [{
+                                        text: '详情',
+                                        link: '/details/others/'
+                                    },
+                                    {
+                                        text: '关键词查询',
+                                        link: '/details/others/keywords/'
+                                    },
+                                    {
+                                        text: '未整理问题',
+                                        link: '/details/others/unorganized/'
+                                    },
                                 ]
                             },
                             {
                                 text: 'iUoB',
-                                items: [
-                                    { text: '下载iUoB', link: '/details/iUoB/download/' },
-                                    { text: '提交问题', link: '/details/iUoB/submit/' },
-                                    { text: '最近更新', link: '/details/iUoB/updates/' }
+                                items: [{
+                                        text: '下载iUoB',
+                                        link: '/details/iUoB/download/'
+                                    },
+                                    {
+                                        text: '提交问题',
+                                        link: '/details/iUoB/submit/'
+                                    },
+                                    {
+                                        text: '最近更新',
+                                        link: '/details/iUoB/updates/'
+                                    }
                                 ]
                             }
 
                         ]
                     },
-                    { text: '联系我们', link: '/contacts/' },
-                    { text: '关于iUoB', link: '/about/' },
+                    {
+                        text: '联系我们',
+                        link: '/contacts/'
+                    },
+                    {
+                        text: '关于iUoB',
+                        link: '/about/'
+                    },
                     {
                         text: '其他',
                         ariaLabel: 'Others Menu',
-                        items: [
-                            { text: 'Homepage - iUoB', link: 'https://www.iuob.uk/' },
-                            { text: 'Status - iUoB', link: 'https://status.iuob.uk/' }
-                        ]
-                    }
-                ],
-                sidebar: 'auto'
-            },
-            '/en-GB/': {
-                label: 'British English',
-                selectText: 'Languages',
-                lastUpdated: 'Last Updated',
-                repo: 'iUoB/help.iuob.uk',
-                repoLabel: 'View source code',
-                editLinks: true,
-                editLinkText: 'Edit this page on GitHub',
-                nav: [
-                    { text: 'UoB Website', link: 'https://www.birmingham.ac.uk/' },
-                    { text: 'Home', link: '/en-GB/' },
-                    { text: 'Help', link: '/en-GB/details/' },
-                    {
-                        text: 'Questions',
-                        ariaLabel: 'Questions Select Menu',
                         items: [{
-                                text: 'Pre-Admissions',
-                                items: [
-                                    { text: 'Check Details', link: '/en-GB/details/pre-admissions/' },
-                                    { text: 'Admissions', link: '/en-GB/details/pre-admissions/admissions/' },
-                                    { text: 'Contacts', link: '/en-GB/details/pre-admissions/contacts/' },
-                                    { text: 'Others', link: '/en-GB/details/pre-admissions/others/' }
-                                ]
+                                text: 'Homepage - iUoB',
+                                link: 'https://www.iuob.uk/'
                             },
                             {
-                                text: 'Enrolled',
-                                items: [
-                                    { text: 'Check Details', link: '/en-GB/details/enrolled/' },
-                                    { text: 'Solutions', link: '/en-GB/details/enrolled/solutions/' },
-                                    { text: 'Others', link: '/en-GB/details/enrolled/others/' }
-                                ]
-                            },
-                            {
-                                text: 'Others',
-                                items: [
-                                    { text: 'Check Details', link: '/en-GB/details/others/' },
-                                    { text: 'Unorganized Questions', link: '/en-GB/details/others/unorganized/' },
-                                ]
-                            },
-                            {
-                                text: 'iUoB',
-                                items: [
-                                    { text: 'Dwonload iUoB', link: '/en-GB/details/iUoB/download/' },
-                                    { text: 'Sumbit Questions', link: '/en-GB/details/iUoB/submit/' },
-                                    { text: 'Recent Updates', link: '/en-GB/details/iUoB/updates/' }
-                                ]
+                                text: 'Status - iUoB',
+                                link: 'https://status.iuob.uk/'
                             }
-
-                        ]
-                    },
-                    { text: 'Contact Us', link: '/en-GB/contacts/' },
-                    { text: 'About iUoB', link: '/en-GB/about/' },
-                    {
-                        text: 'Others',
-                        ariaLabel: 'Others Menu',
-                        items: [
-                            { text: 'Homepage - iUoB', link: 'https://www.iuob.uk/' },
-                            { text: 'Status - iUoB', link: 'https://status.iuob.uk/' }
                         ]
                     }
                 ],
                 sidebar: 'auto'
-            },
+            }
         }
     }
 }
