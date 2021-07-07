a84ee46a0b74fcd44cbb107ed25c3d31951eb31c
Harry Yep <github@hode.uk>
2021-07-07 20:12:18 +0800

    update to v2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,8 @@
 module.exports = {
-    theme: 'default-prefers-color-scheme',
     dest: 'public',
-    markdown: {
-        extendMarkdown: md => {
-            md.set({
-                html: true
-            })
-            md.use(require('markdown-it-katex'))
-        }
-    },
+    lang: 'zh-CN',
+    title: '帮助中心 - iUoB',
+    description: '为你通往伯明翰大学的路上亮起明灯',
     head: [
         ['link', {
             rel: 'icon',
@@ -60,191 +54,163 @@ module.exports = {
             name: 'apple-mobile-web-app-status-bar-style',
             content: 'black'
         }],
-        ['link', {
-            rel: 'stylesheet',
-            href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css'
-        }],
-        ['link', {
-            rel: "stylesheet",
-            href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css"
-        }]
     ],
-    locales: {
-        '/': {
-            lang: 'zh-CN',
-            title: '帮助中心 - iUoB',
-            description: '为你通往伯明翰大学的路上亮起明灯',
-        }
-    },
     plugins: [
-        ['@vuepress/google-analytics', {
-            'ga': 'UA-182989275-1'
-        }],
-        ['@vuepress/back-to-top', true],
-        ['@vuepress/last-updated', {
-            transformer: (timestamp, lang) => {
-                const moment = require("moment");
-                moment.locale(lang);
-                return moment(timestamp).format("YYYY-MM-DD HH:mm");
-            }
-        }],
-        ['@vuepress/medium-zoom', true],
-        ['@vuepress/nprogress', true],
-        ['@vuepress/pwa', {
-            serviceWorker: true,
-            updatePopup: true
-        }],
-        ['pangu', true],
-        ['sitemap', {
-            hostname: 'https://help.iuob.uk'
-        }],
-        ['auto-sidebar', true],
-        ['fulltext-search', true],
-        ['copyright', {
-            noCopy: false,
-            noSelect: false,
-            minLength: 100,
-        }],
+        ['@vuepress/plugin-pwa'],
+        ['@vuepress/plugin-pwa-popup', {
+            locales: {
+                '/': {
+                    message: '发现新内容可用',
+                    buttonText: '刷新',
+                },
+            },
+        }, ],
+        ['@vuepress/plugin-search', {
+            locales: {
+                '/': {
+                    placeholder: '搜索',
+                }
+            },
+        }, ],
+        ['@vuepress/plugin-google-analytics', {
+            id: 'G-NQKDWXZQ9J',
+        }, ],
     ],
     themeConfig: {
-        repo: 'iUoB/help.iuob.uk',
-        editLinks: true,
-        docsDir: 'docs',
         logo: '/iUoB_Logo_RB.png',
-        displayAllHeaders: true,
-        activeHeaderLinks: true,
-        sidebar: 'auto',
-        smoothScroll: true,
-        search: true,
-        searchMaxSuggestions: 10,
-        locales: {
-            '/': {
-                label: '简体中文',
-                selectText: 'Languages',
-                lastUpdated: '最后更新',
-                repo: 'iUoB/help.iuob.uk',
-                repoLabel: '查看源码',
-                editLinks: true,
-                editLinkText: '在GitHub编辑此页面',
-                nav: [{
-                        text: '伯明翰大学官网',
-                        link: 'https://www.birmingham.ac.uk/'
-                    },
-                    {
-                        text: '主页',
-                        link: '/'
-                    },
-                    {
-                        text: '详情',
-                        link: '/details/'
-                    },
-                    {
-                        text: '问题选择',
-                        ariaLabel: '问题选择',
-                        items: [{
-                                text: '入学前',
-                                items: [{
-                                        text: '详情',
-                                        link: '/details/uni/pre-admissions/'
-                                    },
-                                    {
-                                        text: '录取相关',
-                                        link: '/details/uni/pre-admissions/admissions/'
-                                    },
-                                    {
-                                        text: '联系方式',
-                                        link: '/details/uni/pre-admissions/contacts/'
-                                    },
-                                    {
-                                        text: '其他',
-                                        link: '/details/uni/pre-admissions/others/'
-                                    }
-                                ]
+        repo: 'iUoB/help.iuob.uk',
+        tip: '提示',
+        warning: '请注意',
+        danger: '危险提示',
+        editLinkText: '帮助iUoB改善此页面',
+        docsBranch: 'master',
+        docsDir: 'docs',
+        lastUpdatedText: '最后更新',
+        contributors: false,
+        notFound: [
+            '你好像迷失了？？？快离开这里吧。',
+            '看到这条消息的人，今天会有好运发送。',
+            '希望你有美好的一天'
+        ],
+        backToHome: '回到帮助中心 - iUoB',
+        openInNewWindow: '在新窗口打开',
+        navbar: [{
+                text: '伯明翰大学官网',
+                link: 'https://www.birmingham.ac.uk/'
+            },
+            {
+                text: '主页',
+                link: '/'
+            },
+            {
+                text: '详情',
+                link: '/details/'
+            },
+            {
+                text: '问题选择',
+                children: [{
+                        text: '入学前',
+                        children: [{
+                                text: '详情',
+                                link: '/details/pre-admissions/'
                             },
                             {
-                                text: '入学后',
-                                items: [{
-                                        text: '详情',
-                                        link: '/details/uni/enrolled/'
-                                    },
-                                    {
-                                        text: 'Solutions',
-                                        link: '/details/uni/enrolled/solutions/'
-                                    },
-                                    {
-                                        text: '其他',
-                                        link: '/details/uni/enrolled/others/'
-                                    }
-                                ]
+                                text: '录取相关',
+                                link: '/details/pre-admissions/admissions/'
                             },
                             {
-                                text: '其他问题',
-                                items: [{
-                                        text: '详情',
-                                        link: '/details/tips/'
-                                    },
-                                    {
-                                        text: '未整理问题',
-                                        link: '/details/tips/unorganized/'
-                                    },
-                                ]
+                                text: '联系方式',
+                                link: '/details/pre-admissions/contacts/'
                             },
                             {
-                                text: 'iUoB',
-                                items: [{
-                                        text: '下载iUoB',
-                                        link: '/details/iUoB/download/'
-                                    },
-                                    {
-                                        text: '提交问题',
-                                        link: '/details/iUoB/submit/'
-                                    },
-                                    {
-                                        text: '最近更新',
-                                        link: '/details/iUoB/updates/'
-                                    }
-                                ]
-                            }
-
-                        ]
-                    },
-                    {
-                        text: '联系我们',
-                        link: '/contacts/'
-                    },
-                    {
-                        text: '关于iUoB',
-                        link: '/about/'
-                    },
-                    {
-                        text: '其他',
-                        ariaLabel: 'Others Menu',
-                        items: [{
-                                text: 'Homepage - iUoB',
-                                link: 'https://www.iuob.uk/'
-                            },
-                            {
-                                text: 'Status - iUoB',
-                                link: 'https://status.iuob.uk/'
+                                text: '其他',
+                                link: '/details/pre-admissions/others/'
                             }
                         ]
                     },
                     {
-                        text: '语言',
-                        ariaLabel: 'Languages Menu',
-                        items: [{
-                                text: '简体中文',
-                                link: '/'
+                        text: '入学后',
+                        children: [{
+                                text: '详情',
+                                link: '/details/enrolled/'
                             },
                             {
-                                text: 'British English',
-                                link: 'https://en.help.iuob.uk/'
+                                text: 'Solutions',
+                                link: '/details/enrolled/solutions/'
+                            },
+                            {
+                                text: '其他',
+                                link: '/details/enrolled/others/'
+                            }
+                        ]
+                    },
+                    {
+                        text: '其他问题',
+                        children: [{
+                                text: '详情',
+                                link: '/details/others/'
+                            },
+                            {
+                                text: '关键词查询',
+                                link: '/details/others/keywords/'
+                            },
+                            {
+                                text: '未整理问题',
+                                link: '/details/others/unorganized/'
+                            },
+                        ]
+                    },
+                    {
+                        text: 'iUoB',
+                        children: [{
+                                text: '下载iUoB',
+                                link: '/details/iUoB/download/'
+                            },
+                            {
+                                text: '提交问题',
+                                link: '/details/iUoB/submit/'
+                            },
+                            {
+                                text: '最近更新',
+                                link: '/details/iUoB/updates/'
                             }
                         ]
                     }
-                ],
-                sidebar: 'auto'
+
+                ]
+            },
+            {
+                text: '联系我们',
+                link: '/contacts/'
+            },
+            {
+                text: '关于iUoB',
+                link: '/about/'
+            },
+            {
+                text: '其他',
+                children: [{
+                        text: 'Homepage - iUoB',
+                        link: 'https://www.iuob.uk/'
+                    },
+                    {
+                        text: 'Status - iUoB',
+                        link: 'https://status.iuob.uk/'
+                    }
+                ]
+            },
+            {
+                text: '语言',
+                children: [{
+                        text: '简体中文',
+                        link: '/'
+                    },
+                    {
+                        text: 'British English',
+                        link: 'https://en.help.iuob.uk/'
+                    }
+                ]
             }
-        }
-    }
+        ]
+    },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,10 @@
 ---
 home: true
 heroImage: https://cdn.iuob.uk/help/iUoB_Logo_RB.png
-actionText: 阅读更多 →
-actionLink: /details/
+actions:
+  - text: 阅读更多 →
+    link: /details/
+    type: primary
 features:
   - title: 公益性
     details: iUoB 是一个完全的公益项目。
@@ -12,10 +14,6 @@ footer: MIT Licensed | Copyright © 2020-2021 Harry Yep & iUoB Contributors. All
 ---
 
 想要了解更多内容，请访问[英国伯明翰大学官网](https://www.birmingham.ac.uk/)
-
-::: tip
-帮助中心（Beta） - iUoB 现已可供大家测试，访问 <beta.help.iuob.uk> 即可查看
-:::
 
 请选择您的语言进行阅读：
 - [British English](https://en.help.iuob.uk/)
